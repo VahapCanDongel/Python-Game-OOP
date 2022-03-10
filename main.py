758e4794f5cf1f5df2586d1f451f422148c03fd1
VahapCanDongel <vahapcandongel@hotmail.com>
2022-03-10 12:27:47 +0000

    back to inheritence

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
-import time
 pygame.init()
 
 screen = pygame.display.set_mode([1000, 500])
 
         
 class Charactor:
-    def __init__(self, image):
+    def __init__(self, image, x, y):
         self.image = pygame.image.load(image)
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
         
     def move_right(self, speed):
         self.x += speed
@@ -22,13 +21,7 @@ class Charactor:
     
     def move_down(self, speed):
         self.y -= speed
-    
-    def set_x(self, value):
-        self.x  = value
         
-    def set_y(self, value):
-        self.y = value
-    
     def get_x(self):
         return self.x
     
@@ -37,21 +30,20 @@ class Charactor:
         
 
 class Alien(Charactor):
-    def __init__(self, image):
-        super().__init__(image)
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
 
     
 class Player(Charactor):
-    def __init__(self, image):
-        super().__init__(image)
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
 
 
 
 
-alien1 = Alien("assets/alien.png")
-player1 = Player("assets/spaceship.png")
+alien1 = Alien("assets/alien.png", 100, 100)
+player1 = Player("assets/spaceship.png", 700, 250)
 
-alien_position = [[150, 100], [200, 100], [250, 100], [300, 100]]
 
 class Game:
     def __init__(self, screen_height, screen_width, screen_title):
@@ -76,19 +68,7 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         alien1.move_right(4)
                         
-            for item in alien_position:
-                x = item[0]
-                y = item[1]
-                alien1.set_x(x) 
-                alien1.set_y(y)
-           
-                
-                screen.blit(alien1.image, (alien1.get_x(), alien1.get_y()))
-
-        
-            
-                        
-            # screen.blit(alien1.image, (alien1.get_x(), alien1.get_y()))        
+            screen.blit(alien1.image, (alien1.get_x(), alien1.get_y()))        
             screen.blit(player1.image, (player1.get_x(), player1.get_y()))
 
 
